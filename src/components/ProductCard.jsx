8063526ba129efd5e156378774cc68d66b97fe81
Md Siddiqur Rahman <siddikcoder@gmail.com>
2023-05-01 12:02:52 +0600

    add zustand

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card, Col } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 
 function ProductCard(props) {
   let product = props.product;
